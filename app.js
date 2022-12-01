f9d7d27836038289088d660d0cfdaaa6f3830b99
Patrick <kelly.patj@gmail.com>
2022-12-01 15:56:37 -0500

    add basic todo

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("more sanity needed.");
+/*
+Started 12.01.2022
+By: Patrick Kelly
+
+TODOO - Everything, duh.
+  * character object
+    - to include player, npcs?, monsters, etc.
+    - Stats:
+      - Health
+      - Magic
+      - str, dex, etc. (might be too much)
+    - Inventory
+      - Money, items, etc.
+    - status(es)
+    - location & facing
+  * Location object
+    - internal map (maybe? might just be one screen deals)
+    - external 'door' location
+    - what direction the door faces in the overworld
+  * Map and where everything is.
+    - Maybe each character/room/event/thing has a 'location' value? ie: tavern is at [5][3]
+    - 2d array? and keep everything set in a grid.
+    - [0][0] [1][0] [2][0]
+    - [0][1] [1][1] [2][1]  <-- This kind of thing
+    - [0][2] [1][2] [2][2]
+    ? keep a log of where the player has been, and use that as a viewable 'map'?
+  *moar to come.
+
+*/
